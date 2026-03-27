3b34cf7f6485d49579739bb434490563aa994922
Your Name <shapovalnazar2@gmail.com>
2026-03-27 11:46:44 +0100

    Footer updated with layout and scripts

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,17 @@
 <footer>
-    <p>© 2026 My Website</p>
+    <div class="footer-content">
+        <p>© 2026 My Website. All rights reserved.</p>
+        <nav>
+            <a href="index.php">Home</a> |
+            <a href="about.html">About</a> |
+            <a href="contact.html">Contact</a>
+        </nav>
+    </div>
 </footer>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
 <script src="../assets/js/custom.js"></script>
+
 </body>
 </html>
